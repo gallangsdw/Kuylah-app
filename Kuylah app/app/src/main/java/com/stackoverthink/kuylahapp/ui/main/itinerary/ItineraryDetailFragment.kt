84ab60dece81986cdb8f4b91920b8e1218713bfa
nataliosaleky <nataliosaleky@gmail.com>
2021-05-07 01:07:08 +0700

    added fragment itinerary detail

--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/itinerary/ItineraryDetailFragment.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/itinerary/ItineraryDetailFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.stackoverthink.kuylahapp.R
 
-class ItineraryFragment : Fragment() {
+class ItineraryDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_itinerary, container, false)
+        return inflater.inflate(R.layout.fragment_itinerary_detail, container, false)
     }
 
 }
