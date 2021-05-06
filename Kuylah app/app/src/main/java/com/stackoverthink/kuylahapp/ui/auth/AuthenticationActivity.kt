e1781784c93f38e086051ed38c64614e479b718b
gallang sadewa <gallang1800016063@webmail.uad.ac.id>
2021-05-06 21:27:29 +0700

    added bottom nav

--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/auth/AuthenticationActivity.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/auth/AuthenticationActivity.kt
@@ -1,14 +1,24 @@
 package com.stackoverthink.kuylahapp.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.stackoverthink.kuylahapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.stackoverthink.kuylahapp.databinding.ActivityAuthenticationBinding
+import com.stackoverthink.kuylahapp.ui.main.MainActivity
 
 class AuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/MainActivity.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.stackoverthink.kuylahapp.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stackoverthink.kuylahapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        val navController = findNavController(R.id.fragment)
+
+        navView.setupWithNavController(navController)
     }
 }
