2789a11436a223c9e6cb88a359e20b6b976964bc
nataliosaleky <nataliosaleky@gmail.com>
2021-05-07 22:11:16 +0700

    added custom toolbar.xml

--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/MainActivity.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.stackoverthink.kuylahapp.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -9,11 +11,16 @@ import com.stackoverthink.kuylahapp.R
 
 class MainActivity : AppCompatActivity() {
 
+    private var title: String = getString(R.string.home)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
         val navController = findNavController(R.id.fragment)
+
+        val titleActionBar: TextView = findViewById(R.id.tv_title)
+        titleActionBar.text = title
 
         navView.setupWithNavController(navController)
     }
