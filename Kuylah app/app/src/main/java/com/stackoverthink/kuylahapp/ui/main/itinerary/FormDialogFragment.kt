49456a349bd287c7fb42f20209a23ca1237c45d2
nataliosaleky <nataliosaleky@gmail.com>
2021-05-08 16:54:18 +0700

    finished form dialog fragment

--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/itinerary/FormDialogFragment.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/itinerary/FormDialogFragment.kt
@@ -1,15 +1,12 @@
 package com.stackoverthink.kuylahapp.ui.main.itinerary
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.DialogFragment
-import com.stackoverthink.kuylahapp.R
 import com.stackoverthink.kuylahapp.databinding.FragmentFormDialogBinding
-import com.stackoverthink.kuylahapp.databinding.FragmentItineraryBinding
 
 
 class FormDialogFragment : DialogFragment() {
@@ -17,14 +14,25 @@ class FormDialogFragment : DialogFragment() {
     private lateinit var binding: FragmentFormDialogBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentFormDialogBinding.inflate(layoutInflater)
+        if (dialog != null && dialog?.window != null) {
+            dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+            dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE);
+        }
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        val params: ViewGroup.LayoutParams = dialog!!.window!!.attributes
+        params.width = ViewGroup.LayoutParams.WRAP_CONTENT
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.attributes = params as WindowManager.LayoutParams
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
