5311a6fde21d04ba7027374b8bbf2657d2a0b54b
gallang sadewa <gallang1800016063@webmail.uad.ac.id>
2021-05-09 15:04:13 +0700

    connect to firebase, auth user, ngerapiin UI

--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/auth/AuthenticationActivity.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/auth/AuthenticationActivity.kt
@@ -2,13 +2,28 @@ package com.stackoverthink.kuylahapp.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.stackoverthink.kuylahapp.R
 import com.stackoverthink.kuylahapp.databinding.ActivityAuthenticationBinding
 import com.stackoverthink.kuylahapp.ui.main.MainActivity
 
 class AuthenticationActivity : AppCompatActivity() {
 
+    companion object {
+        private const val RC_SIGN_IN = 120
+    }
+
     private lateinit var binding: ActivityAuthenticationBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +32,66 @@ class AuthenticationActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val text = "Harap menunggu gan"
+        val duration = Toast.LENGTH_LONG
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        mAuth = FirebaseAuth.getInstance()
+
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            signIn()
+            Toast.makeText(applicationContext, text, duration).show()
         }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful) {
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("AuthenticationActivity", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("AuthenticationActivity", "Google sign in failed", e)
+                }
+            } else {
+                Log.w("AuthenticationActivity", exception.toString())
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("AuthenticationActivity", "signInWithCredential:success")
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("AuthenticationActivity", "signInWithCredential:failure", task.exception)
+                    }
+                }
     }
 }
--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/splashscreen/SplashActivity.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/splashscreen/SplashActivity.kt
@@ -3,24 +3,39 @@ package com.stackoverthink.kuylahapp.ui.splashscreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
 import com.stackoverthink.kuylahapp.R
 import com.stackoverthink.kuylahapp.ui.auth.AuthenticationActivity
+import com.stackoverthink.kuylahapp.ui.main.MainActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         supportActionBar?.hide()
 
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(2500)
-            startActivity(Intent(this@SplashActivity, AuthenticationActivity::class.java))
-            finish()
-        }
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (user!= null){
+                val homeIntent = Intent(this, MainActivity::class.java)
+                startActivity(homeIntent)
+                finish()
+            }else{
+                val authIntent = Intent(this, AuthenticationActivity::class.java)
+                startActivity(authIntent)
+                finish()
+            }
+        }, 2000)
     }
 }
