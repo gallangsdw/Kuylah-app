948ddc5856638f900aec5e50cea1dbf663637062
nataliosaleky <nataliosaleky@gmail.com>
2021-05-19 11:22:27 +0700

    Recyclerview udated with data from firestore. Masih berantakan.

--- Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/MainActivity.kt
+++ Kuylah app/app/src/main/java/com/stackoverthink/kuylahapp/ui/main/MainActivity.kt
@@ -42,10 +42,7 @@ class MainActivity : AppCompatActivity() {
         var title: TextView = findViewById(R.id.tv_title)
         title.text = getString(R.string.home)
 
-        prepare()
-//        addItem()
-
-//        addScheduleToDatabase(dataSchedule.value, "2")
+        add()
 
         //val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.itineraryFragment, R.id.settingFragment))
         //setupActionBarWithNavController(navController, appBarConfiguration)
@@ -53,20 +50,6 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         navView.setupWithNavController(navController)
-    }
-
-    private fun addScheduleToDatabase(destination: Destination?, day: String) {
-        scheduleField["day"] = day
-        val db = Firebase.firestore
-        db.collection("users/${FirebaseAuth.getInstance().uid}/itineraries").add(scheduleField)
-        db.collection("users/${FirebaseAuth.getInstance().uid}/itineraries/schedule")
-            .add(destination!!)
-            .addOnSuccessListener {
-                Log.d("Itinerary Added Succeed", ": ${destination.toString()}")
-            }
-            .addOnFailureListener {
-                Log.w("Itinerary Added Failed", "Error adding document", it)
-            }
     }
 
     private fun addItineraryToDatabase(
@@ -84,6 +67,7 @@ class MainActivity : AppCompatActivity() {
                 Log.w("Itinerary Added Failed", "Error adding document", it)
             }
 
+        //Adding the itinerary details
         for (position in dataSchedules.indices){
             scheduleField["day"] = day
             db.document("users/${FirebaseAuth.getInstance().uid}/itineraries/${dataItineraries.title.toString()}/schedule/Day 2").set(scheduleField)
@@ -96,109 +80,21 @@ class MainActivity : AppCompatActivity() {
                     Log.w("Itinerary Added Failed", "Error adding document", it)
                 }
         }
-//
-//        scheduleField["day"] = day
-//        db.document("users/${FirebaseAuth.getInstance().uid}/itineraries/${dataItineraries.title.toString()}/schedule/Day 2").set(scheduleField)
-//        db.document("users/${FirebaseAuth.getInstance().uid}/itineraries/${dataItineraries.title.toString()}/schedule/Day 2/destination/${dataSchedules!!.name}")
-//            .set(dataSchedules!!)
-//            .addOnSuccessListener {
-//                Log.d("Itinerary Added Succeed", ": $dataSchedule")
-//            }
-//            .addOnFailureListener {
-//                Log.w("Itinerary Added Failed", "Error adding document", it)
-//            }
     }
 
-    private fun prepare(){
-        dataItineraries.value = Itinerary(title="Ga jadi ke jogja", day = "3", budget = "3000000",category = arrayListOf("Beach", "Natural"))
+    private fun add() {
+        dataItineraries.value = Itinerary(
+            title = "Akhirnya ke jogja lagi",
+            day = "3",
+            budget = "3000000",
+            category = arrayListOf("Beach", "Natural")
+        )
         dataSchedule1.value = Destination(name = "Nasi Goreng", price = "30000")
         dataSchedules.add(dataSchedule1)
         dataSchedule2.value = Destination(name = "Parangtritis", price = "20000")
         dataSchedules.add(dataSchedule2)
         dataSchedule3.value = Destination(name = "UGM", price = "50000")
         dataSchedules.add(dataSchedule3)
-        addItineraryToDatabase(dataItineraries.value, dataSchedules, "2")
-
-
-//        dataItineraries.value = Itinerary(title="Ke jogja lagi akhirnya", day = "2", budget = "3000000",category = arrayListOf("what", "the"), schedule = null)
-
-//        dataTitles = arrayOf(
-//            "Jalan2 ke jogja for the first time!",
-//            "Ke jogja lagi akhirnya",
-//            "Jogjaaaaa",
-//            "Terpaksa ke jogja",
-//            "Ke jogja lagiii",
-//            "Jogja lur"
-//        )
-//
-//        days = arrayOf(
-//            "3 D",
-//            "2 D",
-//            "1 D",
-//            "2 D",
-//            "2 D",
-//            "2 D"
-//        )
-//
-//        dataBudgets = arrayOf(
-//            "Rp500.000",
-//            "Rp400.000",
-//            "Rp800.000",
-//            "Rp120.000",
-//            "Rp50.000",
-//            "Rp60.000"
-//        )
-//
-//        categories = arrayOf(
-//            mutableListOf("Beach", "Natural"),
-//            mutableListOf("Beach", "Natural", "Cultural and Historical"),
-//            mutableListOf("Beach", "Natural"),
-//            mutableListOf("Beach", "Natural"),
-//            mutableListOf("Beach", "Natural"),
-//            mutableListOf("Beach", "Natural", "Museum", "Art Gallery and Exhibition")
-//        )
-//
-//        destinationName = arrayOf(
-//            "Kopi Klotok",
-//            "Bukit Klangon Merapi",
-//            "Situs Warung Boto",
-//        )
-//
-//        destinationTime = arrayOf(
-//            "07:00 AM - 09:00 AM",
-//            "10:00 AM - 12:00 PM",
-//            "01:00 PM - 03:00 PM"
-//        )
-//
-//
-//        for (position in destinationName.indices){
-//            val destinationData = Destination(
-//                destinationName[position],
-//                destinationTime[position]
-//            )
-//            destinations.value
-//        }
-//
-//        val Day = Day(
-//            1,
-//            destinations
-//        )
-//
-//        schedules.add(Day)
-
-
-    }
-
-    private fun addItem(){
-//        for (position in dataTitles.indices){
-//            val itinerary = Itinerary(
-//                dataTitles[position],
-//                days[position],
-//                dataBudgets[position],
-//                categories[position],
-//                schedules
-//            )
-//            dataItineraries.add(itinerary)
-//        }
+        addItineraryToDatabase(dataItineraries.value, dataSchedules, "1")
     }
 }
